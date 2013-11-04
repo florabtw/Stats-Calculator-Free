6e215bb9129ee43dcd9b4d0434ef605b02f3ba75
Nick Pierson <nkcrpn@gmail.com>
2013-11-04 00:50:56 -0600

    Menu items now appear on home screen

--- src/me/nickpierson/StatsCalculator/HomeActivity.java
+++ src/me/nickpierson/StatsCalculator/HomeActivity.java
@@ -2,6 +2,7 @@ package me.nickpierson.StatsCalculator;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
 
 public class HomeActivity extends ActionBarActivity {
 
@@ -17,5 +18,11 @@ public class HomeActivity extends ActionBarActivity {
 		HomePresenter.create(this, model, view);
 
 		setContentView(view.getView());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.home, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
 }
