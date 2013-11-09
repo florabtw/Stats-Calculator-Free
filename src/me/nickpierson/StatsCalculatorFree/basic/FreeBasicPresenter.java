344294c8cc29d5976c95075a4c7b659b93a8ba6c
Nick Pierson <nkcrpn@gmail.com>
2013-11-09 01:57:15 -0600

    Pushes reference screen down to library

--- src/me/nickpierson/StatsCalculatorFree/basic/FreeBasicPresenter.java
+++ src/me/nickpierson/StatsCalculatorFree/basic/FreeBasicPresenter.java
@@ -3,24 +3,12 @@ package me.nickpierson.StatsCalculatorFree.basic;
 import me.nickpierson.StatsCalculator.basic.BasicModel;
 import me.nickpierson.StatsCalculator.basic.BasicPresenter;
 import me.nickpierson.StatsCalculator.basic.BasicView;
-import me.nickpierson.StatsCalculatorFree.reference.FreeBasicReferenceActivity;
 import android.app.Activity;
-import android.content.Intent;
-
-import com.thecellutioncenter.mvplib.ActionListener;
 
 public class FreeBasicPresenter extends BasicPresenter {
 
 	public static void create(final Activity activity, final BasicModel model, final BasicView view) {
 		setup(activity, model, view);
-
-		view.addListener(new ActionListener() {
-
-			@Override
-			public void fire() {
-				activity.startActivity(new Intent(activity, FreeBasicReferenceActivity.class));
-			}
-		}, BasicView.Types.MENU_REFERENCE);
 	}
 
 }
--- test/src/me/nickpierson/StatsCalculatorFree/basic/FreeBasicPresenterTest.java
+++ test/src/me/nickpierson/StatsCalculatorFree/basic/FreeBasicPresenterTest.java
@@ -1,18 +1,10 @@
 package me.nickpierson.StatsCalculatorFree.basic;
 
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import me.nickpierson.StatsCalculator.basic.BasicPresenterTest;
-import me.nickpierson.StatsCalculator.basic.BasicView;
-import me.nickpierson.StatsCalculatorFree.reference.FreeBasicReferenceActivity;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-
-import android.content.Intent;
 
 @Config(manifest = Config.NONE)
 @RunWith(RobolectricTestRunner.class)
@@ -21,16 +13,5 @@ public class FreeBasicPresenterTest extends BasicPresenterTest {
 	@Override
 	public void createPresenter() {
 		FreeBasicPresenter.create(activity, model, view);
-	}
-
-	@Test
-	public void whenMenuReferenceGuideIsClicked_ThenReferenceGuideIsShown() {
-		createPresenter();
-
-		verify(view).addListener(listener.capture(), eq(BasicView.Types.MENU_REFERENCE));
-
-		listener.getValue().fire();
-
-		verify(activity, times(2)).startActivity(new Intent(activity, FreeBasicReferenceActivity.class));
 	}
 }
