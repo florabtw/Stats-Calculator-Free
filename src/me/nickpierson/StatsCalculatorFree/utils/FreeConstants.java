1ea699579cfe260a7a73a5c1b39de82ea12eef94
Nick Pierson <nkcrpn@gmail.com>
2014-02-08 22:03:13 -0600

    Menu option to upgrade to pro now redirects user to the amazon app store

--- src/me/nickpierson/StatsCalculatorFree/utils/FreeConstants.java
+++ src/me/nickpierson/StatsCalculatorFree/utils/FreeConstants.java
@@ -3,6 +3,6 @@ package me.nickpierson.StatsCalculatorFree.utils;
 public class FreeConstants {
 
 	public static final String UPGRADE_ERROR = "Error while trying to open Play Store.";
-	public static final String UPGRADE_URL = "market://details?id=me.nickpierson.StatsCalculatorPro";
+	public static final String UPGRADE_URL = "http://www.amazon.com/gp/mas/dl/android?p=me.nickpierson.StatsCalculatorPro";
 
 }
